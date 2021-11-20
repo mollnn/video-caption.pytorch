e7f3101a7f6b60d31a633caf86da3ee1035f788c
mollnn <1084918073@qq.com>
2021-11-20 12:01:02 +0800

    fix: dim_vid 不匹配

--- models/EncoderRNN.py
+++ models/EncoderRNN.py
@@ -50,6 +50,7 @@ class EncoderRNN(nn.Module):
             - **hidden** (num_layers * num_directions, batch, hidden_size): variable containing the features in the hidden state h
         """
         batch_size, seq_len, dim_vid = vid_feats.size()
+        print(batch_size, seq_len, dim_vid)
         vid_feats = self.vid2hid(vid_feats.view(-1, dim_vid))
         vid_feats = self.input_dropout(vid_feats)
         vid_feats = vid_feats.view(batch_size, seq_len, self.dim_hidden)
